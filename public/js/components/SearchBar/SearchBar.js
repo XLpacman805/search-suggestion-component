6182442ef857e2ed75c4aef9afdbd27fae65cc75
XLpacman805 <xlpacman805@gmail.com>
2021-03-10 21:17:28 -0800

    Created function to render all the suggestions as unordered list

--- public/js/components/SearchBar/SearchBar.js
+++ public/js/components/SearchBar/SearchBar.js
@@ -1,14 +1,51 @@
 class SearchBar extends HTMLElement {
     constructor() {
         super();
+        this.suggestions = ['A', 'B', 'C'];
     }
 
     connectedCallback() {
         this.render();
     }
 
+    /**
+     * Get suggestions list.
+     * @returns array<string>
+     */
+    getSuggestions() {
+        return this.suggestions;
+    }
+
+/**
+ * set suggestion list.
+ * @param {Array<string>} suggestions - A list of suggestions to be displayed.
+ * @returns {void}
+ */
+    setSuggestions(suggestions) {
+        if (Array.isArray(suggestions) && suggestions.every(element => typeof(element) === "string")) {
+            this.suggestions = suggestions;
+        } else {
+            throw new TypeError('Suggestions needs to be an array of strings.');
+        }
+    }
+
+    createUL() {
+        return `
+            <ul>
+                ${this.suggestions.map(suggestion => '<li>' + suggestion + '</li>').join('')}
+            </ul>
+        `;
+    }
+
     render() {
-        this.innerHTML = `HELLO WORLD`;
+        // Create input field
+        // Create dropdown suggestions
+        this.innerHTML = `
+            <form>
+                <input type="text">
+                ${this.createUL()}
+            </form>
+        `;
     }
 }
 customElements.define('jm-search-bar', SearchBar);
